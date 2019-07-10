5c8e28141f9ed5cfa69820367743998763527d40
Lee <1932018667@qq.com>
2019-07-10 10:41:24 +0800

    api修改

--- src/axios/api.js
+++ src/axios/api.js
@@ -26,19 +26,19 @@ export const GET_SERVICE_LIST = 'srvServiceDetail/mine/list' // 获取服务列�
 export const COMMON_SERVICE_LIST = 'srvServiceDetail' // 新增服务 POST 删除服务 DELETE 修改服务 PUT
 export const GET_SERVICE_RECORD = 'srvServiceOrder/mine/list' // 获取服务订单
 export const COMMON_SERVICE_RECORD = 'srvServiceOrder' // 新增服务订单 POST 删除服务订单 DELETE 修改服务 PUT
-/**
- * 订单管理
- */
+    /**
+     * 订单管理
+     */
 export const GET_ORDER_LIST = 'srvServiceOrder/mine/list' // 获取订单列表
 export const UPDATE_ORDER = 'srvServiceOrder' // 修改单个订单
-/**
- * 图片管理
- */
+    /**
+     * 图片管理
+     */
 export const GET_SLIDESHOW_LIST = 'syspic/all' // 获取轮播图列表
 export const EDIT_SLIDESHOW = 'syspic' // 新增 修改 删除 轮播图
-/**
- * 商品管理
- */
+    /**
+     * 商品管理
+     */
 export const GET_GOODS_LIST = 'srvGoods/mine/list' // 获取商品列表
 export const GOODS = 'srvGoods' // 获取商品列表
 export const GET_ONE_GOODS = 'srvGoods/one' // 获取单个商品
